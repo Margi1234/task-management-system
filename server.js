6556268715977aae586729fcea99df0a590e867f
Margi1234 <margi.panjvani123@gmail.com>
2020-04-20 21:21:39 +0530

    modified package.json and server.js for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const path = require("path");
 const app = express();
 const cors = require("cors");
 const keys = require("./config/keys");
@@ -65,6 +66,13 @@ passport.use(
 //   res.send(payload);
 // });
 app.use("/api/users", users);
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = 5000;
 app.listen(port, () => {
   console.log("Listening on PORT ", port, "!");
