e3abc966566ba9c23c71377a8f407b3bec423a3d
Margi1234 <margi.panjvani123@gmail.com>
2020-04-21 18:01:43 +0530

    changed package.json and server.js files

--- server.js
+++ server.js
@@ -66,13 +66,13 @@ passport.use(
 //   res.send(payload);
 // });
 app.use("/api/users", users);
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   //set static folder
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 const port = 5000;
 app.listen(port, () => {
   console.log("Listening on PORT ", port, "!");
